a070fd94d4acf47a7ca8b46d626eb9c9f912588c
Geordon Worley <vadixidav@gmail.com>
2016-06-10 20:07:55 -0400

    add initial command line arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate clap;
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("uarc-asm")
+        .version("0.1.0")
+        .author("Geordon Worley <vadixidav@gmail.com>")
+        .about("A configurable, generic assembler")
+        .arg(Arg::with_name("format")
+            .long("format")
+            .short("f")
+            .takes_value(true)
+            .possible_values(&["little-endian", "big-endian", "hex-list"])
+            .help("The format of the output files"))
+        .arg(Arg::with_name("config")
+            .long("config")
+            .short("c")
+            .help("Input JSON configuration file")
+            .required(true))
+        .arg(Arg::with_name("outputs")
+            .long("outputs")
+            .short("o")
+            .help("List of the output file names"))
+        .arg(Arg::with_name("inputs")
+            .index(1)
+            .help("List of the input assembly files in the order they are parsed"))
+        .get_matches();
 }
