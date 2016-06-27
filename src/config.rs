fd86f0507ce68aa83b1005ec4d438212d6f2591f
Geordon Worley <vadixidav@gmail.com>
2016-06-27 06:34:34 -0400

    completely change all the configs to give more capabilities

--- src/config.rs
+++ src/config.rs
@@ -29,7 +29,7 @@ fn feedback_default_fill_offset() -> isize {
 }
 
 #[derive(Deserialize, Debug)]
-pub struct Feedback {
+pub struct NumFeedback {
     /// Should this be negated before using it?
     #[serde(default="feedback_default_negate")]
     pub negate: bool,
@@ -50,12 +50,48 @@ pub struct Feedback {
     pub fill_offset: isize,
 }
 
+fn tag_feedback_default_relative() -> bool {
+    false
+}
+
+fn tag_feedback_default_shift() -> u32 {
+    0
+}
+
+fn tag_feedback_default_offset() -> isize {
+    0
+}
+
 #[derive(Deserialize, Debug)]
-pub struct Capture {
-    /// The base the number is to be interpreted as.
-    pub base: u32,
-    /// All the places the value is inserted in this ruling.
-    pub feedbacks: Vec<Feedback>,
+pub struct TagFeedback {
+    /// The segment from which to draw the absolute position.
+    pub from_segment: usize,
+    /// Is this tag usage relative?
+    #[serde(default="tag_feedback_default_relative")]
+    pub relative: bool,
+    /// The amount to left shift the absolute position before adding.
+    #[serde(default="tag_feedback_default_shift")]
+    pub shift: u32,
+    /// The segment to add the absolute position to.
+    pub add_segment: usize,
+    /// The index at which to add the absolute position.
+    pub add_index: usize,
+    /// An offset to add.
+    #[serde(default="tag_feedback_default_offset")]
+    pub offset: isize,
+}
+
+#[derive(Deserialize, Debug)]
+pub enum Capture {
+    Tag {
+        feedbacks: Vec<TagFeedback>,
+    },
+    Num {
+        /// The base the number is to be interpreted as.
+        base: u32,
+        /// All the places the value is inserted in this ruling.
+        feedbacks: Vec<NumFeedback>,
+    },
 }
 
 #[derive(Deserialize, Debug)]
@@ -66,6 +102,9 @@ pub struct Rule {
     pub regex: Option<Regex>,
     /// The unmodified values to be inserted in order into each segment of the output.
     pub segment_values: Vec<Vec<u64>>,
+    /// The additions of the absolute position back into the segment values (relative ignored).
+    #[serde(default)]
+    pub self_references: Vec<TagFeedback>,
     /// Capture structs for handling each capture group.
     pub captures: Vec<Capture>,
 }
@@ -79,28 +118,6 @@ pub struct TagCreateRule {
 }
 
 #[derive(Deserialize, Debug)]
-pub struct TagUseFeedback {
-    /// The segment to add the feedback to.
-    pub add_segment: usize,
-    /// Whether or not to use relative position.
-    pub relative: bool,
-    /// The segment to get the position from to add.
-    pub pos_segment: usize,
-    /// An offset to add to the position.
-    pub pos_offset: isize,
-}
-
-#[derive(Deserialize, Debug)]
-pub struct TagUseRule {
-    /// The regex which should have exactly one capture group for the tag.
-    regex_string: String,
-    #[serde(skip_deserializing)]
-    pub regex: Option<Regex>,
-    /// Feedback of the tag's data into the segments.
-    pub feedbacks: Vec<TagUseFeedback>,
-}
-
-#[derive(Deserialize, Debug)]
 pub struct Config {
     /// The widths of each output segment in octets.
     pub segment_widths: Vec<usize>,
@@ -108,8 +125,6 @@ pub struct Config {
     pub split_whitespace: bool,
     /// The rule for creating tags.
     pub tag_create: TagCreateRule,
-    /// The rule for using tags.
-    pub tag_use_rules: Vec<TagUseRule>,
     /// The rules for everything else.
     pub rules: Vec<Rule>,
 }
@@ -138,30 +153,30 @@ impl Config {
         if self.tag_create.regex.as_ref().unwrap().captures_len() != 2 {
             panic!("Error: The tag create regex must always have one capture group for the tag.");
         }
-        for tag_use in &mut self.tag_use_rules {
-            tag_use.regex = Some(Regex::new(&tag_use.regex_string).unwrap_or_else(|e| {
-                panic!("Error: Failed to parse tag use regex \"{}\": {}",
-                       tag_use.regex_string,
-                       e)
-            }));
-            if tag_use.regex.as_ref().unwrap().captures_len() != 2 {
-                panic!("Error: The tag use regex \"{}\" must always have one capture group for \
-                        the tag.",
-                       tag_use.regex_string);
-            }
-            for feedback in &tag_use.feedbacks {
-                if feedback.add_segment >= self.segment_widths.len() {
-                    panic!("Error: A feedback in the tag use \"{}\" struct uses a non-existent \
-                            add segment.",
-                           tag_use.regex_string);
-                }
-                if feedback.pos_segment >= self.segment_widths.len() {
-                    panic!("Error: A feedback in the tag use \"{}\" struct uses a non-existent \
-                            pos segment.",
-                           tag_use.regex_string);
-                }
-            }
-        }
+        // for tag_use in &mut self.tag_use_rules {
+        // tag_use.regex = Some(Regex::new(&tag_use.regex_string).unwrap_or_else(|e| {
+        // panic!("Error: Failed to parse tag use regex \"{}\": {}",
+        // tag_use.regex_string,
+        // e)
+        // }));
+        // if tag_use.regex.as_ref().unwrap().captures_len() != 2 {
+        // panic!("Error: The tag use regex \"{}\" must always have one capture group for \
+        // the tag.",
+        // tag_use.regex_string);
+        // }
+        // for feedback in &tag_use.feedbacks {
+        // if feedback.add_segment >= self.segment_widths.len() {
+        // panic!("Error: A feedback in the tag use \"{}\" struct uses a non-existent \
+        // add segment.",
+        // tag_use.regex_string);
+        // }
+        // if feedback.pos_segment >= self.segment_widths.len() {
+        // panic!("Error: A feedback in the tag use \"{}\" struct uses a non-existent \
+        // pos segment.",
+        // tag_use.regex_string);
+        // }
+        // }
+        // }
         for rule in &mut self.rules {
             let segment_counts = rule.segment_values.iter().map(|v| v.len()).collect_vec();
             if segment_counts.len() != self.segment_widths.len() {
@@ -172,23 +187,71 @@ impl Config {
                 .unwrap_or_else(|e| panic!("Error: Failed to parse regex: {}", e)));
             if rule.regex.as_ref().unwrap().captures_len() - 1 != rule.captures.len() {
                 panic!("Error: Rule \"{}\" has a different amount of capture structs than its \
-                        regex has captures.");
+                        regex has captures.",
+                       rule.regex_string);
+            }
+            for self_reference in &rule.self_references {
+                if self_reference.from_segment >= self.segment_widths.len() {
+                    panic!("Error: Rule \"{}\" attempts to self-reference an invalid segment {}.",
+                           rule.regex_string,
+                           self_reference.from_segment);
+                }
+                if self_reference.add_segment >= segment_counts.len() {
+                    panic!("Error: Rule \"{}\" attempts to add a self-reference to an invalid \
+                            segment {}.",
+                           rule.regex_string,
+                           self_reference.add_segment);
+                }
+                if self_reference.add_index >= segment_counts[self_reference.add_segment] {
+                    panic!("Error: Rule \"{}\" attempts to add a self-reference to an invalid \
+                            index {} of segment {}.",
+                           rule.regex_string,
+                           self_reference.add_index,
+                           self_reference.add_segment);
+                }
             }
             for capture in &rule.captures {
-                for feedback in &capture.feedbacks {
-                    let count = *segment_counts.get(feedback.segment)
-                        .unwrap_or_else(|| {
-                            panic!("Error: Rule \"{}\" attempts to access invalid segment {}.",
-                                   rule.regex_string,
-                                   feedback.segment)
-                        });
-
-                    if feedback.index >= count {
-                        panic!("Error: Rule \"{}\" attempts to access invalid segment value \
-                                {}:{}.",
-                               rule.regex_string,
-                               feedback.segment,
-                               feedback.index);
+                match capture {
+                    &Capture::Tag { ref feedbacks } => {
+                        for feedback in feedbacks {
+                            if feedback.from_segment >= segment_counts.len() {
+                                panic!("Error: Rule \"{}\" attempts to access invalid tag \
+                                        segment {}.",
+                                       rule.regex_string,
+                                       feedback.from_segment);
+                            }
+                            if feedback.add_segment >= segment_counts.len() {
+                                panic!("Error: Rule \"{}\" attempts to access invalid feedback \
+                                        segment {}.",
+                                       rule.regex_string,
+                                       feedback.add_segment);
+                            }
+                            if feedback.add_index >= segment_counts[feedback.add_segment] {
+                                panic!("Error: Rule \"{}\" attempts to access invalid index {} \
+                                        in segment {}.",
+                                       rule.regex_string,
+                                       feedback.add_index,
+                                       feedback.add_segment);
+                            }
+                        }
+                    }
+                    &Capture::Num { ref feedbacks, .. } => {
+                        for feedback in feedbacks {
+                            let count = *segment_counts.get(feedback.segment)
+                                .unwrap_or_else(|| {
+                                    panic!("Error: Rule \"{}\" attempts to access invalid \
+                                            segment {}.",
+                                           rule.regex_string,
+                                           feedback.segment);
+                                });
+                            if feedback.index >= count {
+                                panic!("Error: Rule \"{}\" attempts to access invalid segment \
+                                        value {}:{}.",
+                                       rule.regex_string,
+                                       feedback.segment,
+                                       feedback.index);
+                            }
+                        }
                     }
                 }
             }
--- src/parse.rs
+++ src/parse.rs
@@ -1,4 +1,4 @@
-use super::config::Config;
+use super::config::{Config, Capture};
 use std::collections::BTreeMap;
 use std::io::{BufRead, Write};
 
@@ -56,7 +56,7 @@ impl<'a> Parser<'a> {
                        r.tag,
                        r.line);
             });
-            self.segments[r.add_segment][r.index] =
+            self.segments[r.add_segment][r.index] +=
                 (tag[r.pos_segment] as isize + r.pos_offset) as u64;
         }
     }
@@ -140,10 +140,7 @@ impl<'a> Parser<'a> {
         if self.attempt_tag_create(segment) {
             return;
         }
-        if self.attempt_tag_use(segment, line) {
-            return;
-        }
-        if self.attempt_rules(segment) {
+        if self.attempt_rules(segment, line) {
             return;
         }
         panic!("Error: Unrecognized symbol \"{}\" on line {}.",
@@ -164,69 +161,67 @@ impl<'a> Parser<'a> {
         }
     }
 
-    fn attempt_tag_use(&mut self, segment: &str, line: usize) -> bool {
-        let config = &self.config;
-        for rule in &config.tag_use_rules {
-            if let Some(caps) = rule.regex.as_ref().unwrap().captures(segment) {
-                let tag = caps.at(1).unwrap().to_string();
-                for feedback in &rule.feedbacks {
-                    let index = self.segments[feedback.add_segment].len();
-                    let current_pos_index = self.segments[feedback.pos_segment].len();
-                    self.replacements.push(Replacement {
-                        line: line,
-                        add_segment: feedback.add_segment,
-                        index: index,
-                        tag: tag.clone(),
-                        pos_segment: feedback.pos_segment,
-                        pos_offset: if feedback.relative {
-                            feedback.pos_offset - current_pos_index as isize
-                        } else {
-                            feedback.pos_offset
-                        },
-                    });
-
-                    // Add the padding
-                    self.segments[feedback.add_segment].push(0);
-                }
-                return true;
-            }
-        }
-        false
-    }
-
-    fn attempt_rules(&mut self, segment: &str) -> bool {
+    fn attempt_rules(&mut self, segment: &str, line: usize) -> bool {
         let config = &self.config;
         for rule in &config.rules {
             if let Some(caps) = rule.regex.as_ref().unwrap().captures(segment) {
                 let mut segvals = rule.segment_values.clone();
+                for self_reference in &rule.self_references {
+                    segvals[self_reference.add_segment][self_reference.add_index] +=
+                        (self.segments[self_reference.from_segment].len() as u64) <<
+                        self_reference.shift;
+                }
                 for (index, capture) in rule.captures.iter().enumerate() {
                     use std::mem::transmute;
                     let cap_string = caps.at(index + 1).unwrap();
-                    let pval = i64::from_str_radix(cap_string, capture.base).unwrap_or_else(|e| {
-                        panic!("Error: Failed to parse captured string \"{}\" from \"{}\": {}",
-                               cap_string,
-                               segment,
-                               e);
-                    });
-                    let val: u64 = unsafe { transmute(pval) };
-
-                    for feedback in &capture.feedbacks {
-                        let mut shiftval = val << feedback.shift;
-                        if feedback.negate {
-                            shiftval = !shiftval + 1;
+                    match capture {
+                        &Capture::Tag { ref feedbacks } => {
+                            for feedback in feedbacks {
+                                self.replacements.push(Replacement {
+                                    line: line,
+                                    add_segment: feedback.add_segment,
+                                    index: self.segments[feedback.add_segment].len() +
+                                           feedback.add_index,
+                                    tag: String::from(cap_string),
+                                    pos_segment: feedback.from_segment,
+                                    pos_offset: if feedback.relative {
+                                        feedback.offset -
+                                        self.segments[feedback.from_segment].len() as isize
+                                    } else {
+                                        feedback.offset
+                                    },
+                                });
+                            }
                         }
-                        if feedback.fill {
-                            let baseval = segvals[feedback.segment][feedback.index];
-                            let fill_amount = shiftval as isize + feedback.fill_offset;
-                            if fill_amount.is_negative() {
-                                panic!("Error: Got a negative fill amount!");
-                            } else {
-                                for _ in 0..fill_amount {
-                                    self.segments[feedback.segment].push(baseval);
+                        &Capture::Num { ref feedbacks, ref base } => {
+                            let pval = i64::from_str_radix(cap_string, *base).unwrap_or_else(|e| {
+                                panic!("Error: Failed to parse captured string \"{}\" from \
+                                        \"{}\": {}",
+                                       cap_string,
+                                       segment,
+                                       e);
+                            });
+                            let val: u64 = unsafe { transmute(pval) };
+
+                            for feedback in feedbacks {
+                                let mut shiftval = val << feedback.shift;
+                                if feedback.negate {
+                                    shiftval = !shiftval + 1;
+                                }
+                                if feedback.fill {
+                                    let baseval = segvals[feedback.segment][feedback.index];
+                                    let fill_amount = shiftval as isize + feedback.fill_offset;
+                                    if fill_amount.is_negative() {
+                                        panic!("Error: Got a negative fill amount!");
+                                    } else {
+                                        for _ in 0..fill_amount {
+                                            self.segments[feedback.segment].push(baseval);
+                                        }
+                                    }
+                                } else {
+                                    segvals[feedback.segment][index] += shiftval;
                                 }
                             }
-                        } else {
-                            segvals[feedback.segment][index] += shiftval;
                         }
                     }
                 }
